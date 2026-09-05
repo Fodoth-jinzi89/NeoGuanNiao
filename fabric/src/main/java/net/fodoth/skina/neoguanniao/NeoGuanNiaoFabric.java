706455716fb27a2ec6763b22eeee03ed77ad73d0
Fodoth_jinzi89 <wlcypr222@126.com>
2026-09-05 20:09:35 +0800

    feat: add fabric platform entrypoint

---
+++ fabric/src/main/java/net/fodoth/skina/neoguanniao/NeoGuanNiaoFabric.java
@@ -0,0 +1,9 @@
+package net.fodoth.skina.neoguanniao;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class NeoGuanNiaoFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ fabric/src/main/java/net/fodoth/skina/neoguanniao/platform/ClientHooksImpl.java
@@ -0,0 +1,9 @@
+package net.fodoth.skina.neoguanniao.platform;
+
+import net.minecraft.world.item.ItemStack;
+
+public final class ClientHooksImpl {
+    private ClientHooksImpl() {}
+    public static void openBirdGuide(ItemStack stack) {
+    }
+}
