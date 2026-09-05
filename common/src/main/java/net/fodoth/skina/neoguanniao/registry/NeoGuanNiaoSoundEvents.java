51fd41baec6eefc61f27c2d6624cc72e5a774663
Fodoth_jinzi89 <wlcypr222@126.com>
2026-09-05 19:39:27 +0800

    refactor: migrate sound event registry

--- common/src/main/java/net/fodoth/skina/neoguanniao/registry/NeoGuanNiaoSoundEvents.java
+++ common/src/main/java/net/fodoth/skina/neoguanniao/registry/NeoGuanNiaoSoundEvents.java
@@ -4,72 +4,71 @@ import net.fodoth.skina.neoguanniao.NeoGuanNiao;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import dev.architectury.registry.registries.Registrar;
 
 public final class NeoGuanNiaoSoundEvents {
-    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-            DeferredRegister.create(Registries.SOUND_EVENT, NeoGuanNiao.MODID);
+    public static final Registrar<SoundEvent> SOUND_EVENTS = NeoGuanNiaoRegistrar.MANAGER.get(Registries.SOUND_EVENT);
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_BURIAL_VORTEX = register("item.feather_fan.burial_vortex");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_BURIAL_SLASH = register("item.feather_fan.burial_slash");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_RIVEN_PIN = register("item.feather_fan.riven_pin");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_RIVEN_SPLIT = register("item.feather_fan.riven_split");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_RIVEN_LOCK = register("item.feather_fan.riven_lock");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_RIVEN_BURST = register("item.feather_fan.riven_burst");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_HUNT_LOCK = register("item.feather_fan.hunt_lock");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_HUNT_START = register("item.feather_fan.hunt_start");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_HUNT_TURN = register("item.feather_fan.hunt_turn");
-    public static final DeferredHolder<SoundEvent, SoundEvent> FEATHER_FAN_HUNT_HIT = register("item.feather_fan.hunt_hit");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_BURIAL_VORTEX = register("item.feather_fan.burial_vortex");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_BURIAL_SLASH = register("item.feather_fan.burial_slash");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_RIVEN_PIN = register("item.feather_fan.riven_pin");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_RIVEN_SPLIT = register("item.feather_fan.riven_split");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_RIVEN_LOCK = register("item.feather_fan.riven_lock");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_RIVEN_BURST = register("item.feather_fan.riven_burst");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_HUNT_LOCK = register("item.feather_fan.hunt_lock");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_HUNT_START = register("item.feather_fan.hunt_start");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_HUNT_TURN = register("item.feather_fan.hunt_turn");
+    public static final RegistrySupplier<SoundEvent> FEATHER_FAN_HUNT_HIT = register("item.feather_fan.hunt_hit");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> NIGHT_HERON_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> NIGHT_HERON_AMBIENT =
             register("entity.night_heron.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> NIGHT_HERON_HURT =
+    public static final RegistrySupplier<SoundEvent> NIGHT_HERON_HURT =
             register("entity.night_heron.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> NIGHT_HERON_DEATH =
+    public static final RegistrySupplier<SoundEvent> NIGHT_HERON_DEATH =
             register("entity.night_heron.death");
-    public static final DeferredHolder<SoundEvent, SoundEvent> NIGHT_HERON_ATTACK =
+    public static final RegistrySupplier<SoundEvent> NIGHT_HERON_ATTACK =
             register("entity.night_heron.attack");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPARROW_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> SPARROW_AMBIENT =
             register("entity.sparrow.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPARROW_HURT =
+    public static final RegistrySupplier<SoundEvent> SPARROW_HURT =
             register("entity.sparrow.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPARROW_DEATH =
+    public static final RegistrySupplier<SoundEvent> SPARROW_DEATH =
             register("entity.sparrow.death");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> BUDGERIGAR_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> BUDGERIGAR_AMBIENT =
             register("entity.budgerigar.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> BUDGERIGAR_HURT =
+    public static final RegistrySupplier<SoundEvent> BUDGERIGAR_HURT =
             register("entity.budgerigar.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> BUDGERIGAR_DEATH =
+    public static final RegistrySupplier<SoundEvent> BUDGERIGAR_DEATH =
             register("entity.budgerigar.death");
-    public static final DeferredHolder<SoundEvent, SoundEvent> BUDGERIGAR_INTERACT =
+    public static final RegistrySupplier<SoundEvent> BUDGERIGAR_INTERACT =
             register("entity.budgerigar.interact");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPOTTED_DOVE_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> SPOTTED_DOVE_AMBIENT =
             register("entity.spotted_dove.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPOTTED_DOVE_HURT =
+    public static final RegistrySupplier<SoundEvent> SPOTTED_DOVE_HURT =
             register("entity.spotted_dove.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPOTTED_DOVE_DEATH =
+    public static final RegistrySupplier<SoundEvent> SPOTTED_DOVE_DEATH =
             register("entity.spotted_dove.death");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPOTTED_DOVE_MATE =
+    public static final RegistrySupplier<SoundEvent> SPOTTED_DOVE_MATE =
             register("entity.spotted_dove.mate");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> PIGEON_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> PIGEON_AMBIENT =
             register("entity.pigeon.ambient");
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> KIWI_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> KIWI_AMBIENT =
             register("entity.kiwi.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> KIWI_HURT =
+    public static final RegistrySupplier<SoundEvent> KIWI_HURT =
             register("entity.kiwi.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> KIWI_DEATH =
+    public static final RegistrySupplier<SoundEvent> KIWI_DEATH =
             register("entity.kiwi.death");
-    public static final DeferredHolder<SoundEvent, SoundEvent> MYNA_AMBIENT =
+    public static final RegistrySupplier<SoundEvent> MYNA_AMBIENT =
             register("entity.myna.ambient");
-    public static final DeferredHolder<SoundEvent, SoundEvent> MYNA_HURT =
+    public static final RegistrySupplier<SoundEvent> MYNA_HURT =
             register("entity.myna.hurt");
-    public static final DeferredHolder<SoundEvent, SoundEvent> MYNA_DEATH =
+    public static final RegistrySupplier<SoundEvent> MYNA_DEATH =
             register("entity.myna.death");
 
 
@@ -77,7 +76,7 @@ public final class NeoGuanNiaoSoundEvents {
     }
 
 
-    private static DeferredHolder<SoundEvent, SoundEvent> register(String id) {
+    private static RegistrySupplier<SoundEvent> register(String id) {
         return SOUND_EVENTS.register(
                 id,
                 () -> SoundEvent.createVariableRangeEvent(
@@ -86,3 +85,4 @@ public final class NeoGuanNiaoSoundEvents {
         );
     }
 }
+
