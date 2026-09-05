02fdf7576129f84ac9f09bce0f4ddf3c991d2230
Fodoth_jinzi89 <wlcypr222@126.com>
2026-09-05 20:13:06 +0800

    feat: register common content on fabric

--- fabric/src/main/java/net/fodoth/skina/neoguanniao/NeoGuanNiaoFabric.java
+++ fabric/src/main/java/net/fodoth/skina/neoguanniao/NeoGuanNiaoFabric.java
@@ -1,9 +1,18 @@
 package net.fodoth.skina.neoguanniao;
 
 import net.fabricmc.api.ModInitializer;
+import net.fodoth.skina.neoguanniao.registry.*;
 
 public final class NeoGuanNiaoFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        NeoGuanNiaoBlocks.BLOCKS.register();
+        NeoGuanNiaoItems.ITEMS.register();
+        NeoGuanNiaoSoundEvents.SOUND_EVENTS.register();
+        NeoGuanNiaoRecipeSerializers.RECIPE_SERIALIZERS.register();
+        NeoGuanNiaoEntityTypes.ENTITY_TYPES.register();
+        NeoGuanNiaoBlockEntityTypes.BLOCK_ENTITY_TYPES.register();
+        NeoGuanNiaoParticleTypes.PARTICLE_TYPES.register();
+        NeoGuanNiaoCreativeTabs.CREATIVE_MODE_TABS.register();
     }
 }
